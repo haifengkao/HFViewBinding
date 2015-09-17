ec1b46996e3cef201f49c5f7479f649cb3f926a0
Hai Feng Kao <haifeng@cocoaspice.in>
2015-09-17 17:33:29 +0800

    - add fast prototyping functions

--- Pod/Classes/HFMetaBindingHelper.m
+++ Pod/Classes/HFMetaBindingHelper.m
@@ -16,6 +16,14 @@ static inline id safe_cast_helper(id x, Class c) {
 }
 #endif
 
+#import <Foundation/Foundation.h>
+@interface NSObject (NSObject_KVCExtensions)
+
+- (BOOL)canSetValueForKey:(NSString *)key;
+- (BOOL)canSetValueForKeyPath:(NSString *)keyPath;
+
+@end
+
 @interface HFMetaBindingHelper()
 @property (nonatomic, strong) AMBlockToken* primaryToken;
 @property (nonatomic, strong) NSMutableArray* secondaryTokens;
@@ -201,8 +209,12 @@ static inline id safe_cast_helper(id x, Class c) {
     id<HFBindingViewDelegate> cell = [self dequeueReusableCellWithIndexPath:indexPath];
     
     id item = [self itemAtIndexPath:indexPath];
-    if (item && [cell conformsToProtocol:@protocol(HFBindingViewDelegate)]) {
-        [cell bindModel:item];
+    if (item && cell) {
+        if ([cell conformsToProtocol:@protocol(HFBindingViewDelegate)]) {
+            [cell bindModel:item];
+        } else if ([item isKindOfClass:[NSDictionary class]]){
+            [self defaultBindingToCell:cell model:item];
+        }
     }
     return cell;
 }
@@ -294,4 +306,89 @@ static inline id safe_cast_helper(id x, Class c) {
     }
     [tokens removeObjectsAtIndexes:indexes];
 }
+
+#pragma mark - prototye helpers
+- (void)defaultBindingToCell:(id)object model:(NSDictionary*)model
+{
+    [model enumerateKeysAndObjectsUsingBlock:^(id  _Nonnull theKey, id  _Nonnull theObj, BOOL * _Nonnull stop) {
+        if ([theKey isKindOfClass:[NSString class]]) {
+            NSString* key = theKey;
+            id obj = theObj;
+            // TODO: check the target object type
+            if ([object canSetValueForKeyPath:key]) {
+                if ([key containsString:@".image"]) {
+                    // check the image path
+                    obj = [UIImage imageNamed:theObj];
+                }
+                [object setValue:obj forKeyPath:key];
+            }
+        }
+    }];
+}
+@end
+
+#import <objc/runtime.h>
+
+@implementation NSObject (NSObject_KVCExtensions)
+
+
+// Can set value for key follows the Key Value Settings search pattern as defined
+// in the apple documentation
+- (BOOL)canSetValueForKey:(NSString *)key {
+    // Check if there is a selector based setter
+    NSString *capKey = [key stringByReplacingCharactersInRange:NSMakeRange(0, 1) withString:[[key substringToIndex:1] uppercaseString]];
+    SEL setter = NSSelectorFromString([NSString stringWithFormat:@"set%@:", capKey]);
+    if ([self respondsToSelector:setter]) {
+        return YES;
+    }
+    
+    // If you can access the instance variable directly, check if that exists
+    // Patterns for instance variable naming:
+    //  1. _<key>
+    //  2. _is<Key>
+    //  3. <key>
+    //  4. is<Key>
+    if ([[self class] accessInstanceVariablesDirectly]) {
+        // Declare all the patters for the key
+        const char *pattern1 = [[NSString stringWithFormat:@"_%@",key] UTF8String];
+        const char *pattern2 = [[NSString stringWithFormat:@"_is%@",capKey] UTF8String];
+        const char *pattern3 = [[NSString stringWithFormat:@"%@",key] UTF8String];
+        const char *pattern4 = [[NSString stringWithFormat:@"is%@",capKey] UTF8String];
+        
+        unsigned int numIvars = 0;
+        Ivar *ivarList = class_copyIvarList([self class], &numIvars);
+        for (unsigned int i = 0; i < numIvars; i++) {
+            const char *name = ivar_getName(*ivarList);
+            if (strcmp(name, pattern1) == 0 ||
+                strcmp(name, pattern2) == 0 ||
+                strcmp(name, pattern3) == 0 ||
+                strcmp(name, pattern4) == 0) {
+                return YES;
+            }
+            ivarList++;
+        }
+    }
+    
+    return NO;
+}
+
+// Traverse the key path finding you can set the values
+// Keypath is a set of keys delimited by "."
+- (BOOL)canSetValueForKeyPath:(NSString *)keyPath {
+    NSRange delimeterRange = [keyPath rangeOfCharacterFromSet:[NSCharacterSet characterSetWithCharactersInString:@"."]];
+    
+    if (delimeterRange.location == NSNotFound) {
+        return [self canSetValueForKey:keyPath];
+    }
+    
+    NSString *first = [keyPath substringToIndex:delimeterRange.location];
+    NSString *rest = [keyPath substringFromIndex:(delimeterRange.location + 1)];
+    
+    if ([self canSetValueForKey:first]) {
+        return [[self valueForKey:first] canSetValueForKeyPath:rest];
+    }
+    
+    return NO;
+}
+
 @end
